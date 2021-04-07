31868775c2add8226a10a6106e70f00782656ed6
JMarcell <79013263+Ganjaa1@users.noreply.github.com>
2021-04-07 10:23:40 +0200

    Add files via upload

---
+++ O9V4M0_0331/O9V4M0_alarmra_var.c
@@ -0,0 +1,14 @@
+
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+
+void do_nothing();
+
+int main(){
+signal(SIGALRM, do_nothing);
+printf("varok!\n");
+pause();
+printf("Vegre megjott az alarm!\n");
+}
+void do_nothing(){;}
